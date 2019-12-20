f4cd3cf05b0b614a0cfcc4785178af6b8a091005
ZGH0717 <583526930@qq.com>
2019-12-20 18:02:04 +0800

    dev

--- packages/Cell/main.vue
+++ packages/Cell/main.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="btn hua-cell borderB" @click="handle">
+  <div class="btn hua-cell" @click="handle" :class="{borderB:border}">
     <slot></slot>
-    <div v-if="isArrow" class="iconR">
+    <div v-if="isArrow" class="absoluteRC icon-r">
       <span class="hua-arrow" :class="{ active: isActive }">
         <slot name="iconR">
-          <i class="iconfont ">&#xe6a7;</i>
+          <i class="iconfont">&#xe6a7;</i>
         </slot>
       </span>
     </div>
@@ -22,6 +22,10 @@ export default {
     isRoate: {
       type: Boolean,
       default: false
+    },
+    border: {
+      type: Boolean,
+      default: true
     }
   },
   data () {
